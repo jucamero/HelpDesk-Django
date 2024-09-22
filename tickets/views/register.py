767c5c9aa6adc330d3a8dc631c8b4511877e04ba
juan camilo mendoza robledo <juancamilo042911@gmail.com>
2024-09-22 18:07:12 -0500

    Se agrega estilos finales en todas las vistas

--- tickets/views/register.py
+++ tickets/views/register.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from ..forms import CustomUserCreationForm
 
@@ -7,9 +8,16 @@ def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home_user')  # O redirige donde prefieras
+            password1 = form.cleaned_data.get('password1')
+            password2 = form.cleaned_data.get('password2')
+            if password1 != password2:
+                messages.error(request, "Las contraseñas no coinciden.")
+            else:
+                user = form.save()
+                login(request, user)
+                return redirect('home_user')  # O redirige donde prefieras
+        else:
+            messages.error(request, "Error en el formulario.")
     else:
         form = CustomUserCreationForm()
     return render(request, 'tickets/register.html', {'form': form})
